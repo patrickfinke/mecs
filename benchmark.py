66a98d2e562486b9fe8ad482ae49d8d9a141abff
Patrick Finke <patrick_finke@gmx.de>
2020-06-17 15:49:27 +0200

    Rewrite benchmarks for more informative output

--- benchmark.py
+++ benchmark.py
@@ -1,5 +1,6 @@
 import sys, time, argparse, random
 from mecs import Scene, CommandBuffer
+import mecs
 
 try:
     import esper
@@ -7,34 +8,14 @@ except ImportError:
     print("WARNING: For running benchmarks on the 'esper' backend, install the necessary package.")
 
 def mean(values):
-    """Calculate the mean value."""
+    """Calculate sample mean."""
 
     return sum(values) / len(values)
 
-def benchmark(backend, task, specific, restarts=10, metric=mean, ndigits=4, setup=None):
-    """Benchmark a function."""
+def variance(values, mean):
+    """Calculate sample variance."""
 
-    def timer(function):
-        def wrapper(hparams, verbose=False):
-            times = []
-            for _ in range(restarts):
-                setup_result = setup(hparams) if setup is not None else Scene()
-
-                start = time.process_time()
-                function(setup_result, hparams)
-                end = time.process_time()
-                times.append(end - start)
-                
-            result = metric(times)
-            if verbose:
-                print(f"{backend}/{task}/{specific}: {round(result, ndigits)} seconds")
-            return result
-
-        wrapper.backend = backend
-        wrapper.task = task
-        wrapper.specific = specific
-        return wrapper
-    return timer
+    return sum(map(lambda v: (v - mean)**2, values)) / len(values)
 
 class Position():
     def __init__(self, x=0.0, y=0.0):
@@ -47,6 +28,9 @@ class Velocity():
 class Lifetime():
     def __init__(self, timer=1.0):
         self.timer = timer
+
+def mecs_setup_none(hparams):
+    return Scene()
 
 def mecs_setup_empty(hparams):
     scene = Scene()
@@ -93,173 +77,142 @@ def mecs_setup_mixed(hparams):
     return scene, eids
 
 
-@benchmark("mecs", "create", "0 components", setup=lambda hparams: Scene())
 def mecs_create_entities(scene, hparams):
     for _ in range(hparams.count):
         scene.new()
 
-@benchmark("mecs", "create", "1 component", setup=lambda hparams: Scene())
 def mecs_create_entities_one_component(scene, hparams):
     for _ in range(hparams.count):
         scene.new(Position())
 
-@benchmark("mecs", "create", "2 components", setup=lambda hparams: Scene())
 def mecs_create_entities_two_components(scene, hparams):
     for _ in range(hparams.count):
         scene.new(Position(), Velocity())
 
-@benchmark("mecs", "create", "3 components", setup=lambda hparams: Scene())
 def mecs_create_entities_three_components(scene, hparams):
     for _ in range(hparams.count):
         scene.new(Position(), Velocity(), Lifetime())
 
-@benchmark("mecs", "query", "all entities", setup=mecs_setup_full)
 def mecs_query_all_entities(setup, hparams):
     scene, _ = setup
     for _ in scene.select():
         pass
 
-@benchmark("mecs", "query", "1 component", setup=mecs_setup_mixed)
 def mecs_query_one_component(setup, hparams):
     scene, _ = setup
     for _ in scene.select(Position):
         pass
 
-@benchmark("mecs", "query", "2 components", setup=mecs_setup_mixed)
 def mecs_query_two_components(setup, hparams):
     scene, _ = setup
     for _ in scene.select(Position, Velocity):
         pass
 
-@benchmark("mecs", "query", "3 components", setup=mecs_setup_mixed)
 def mecs_query_three_components(setup, hparams):
     scene, _ = setup
     for _ in scene.select(Position, Velocity, Lifetime):
         pass
 
-@benchmark("mecs", "add", "1 component", setup=mecs_setup_empty)
 def mecs_add_one_component(setup, hparams):
     scene, eids = setup
     for eid in eids:
         scene.set(eid, Position())
 
-@benchmark("mecs", "add", "2 components", setup=mecs_setup_empty)
 def mecs_add_two_components(setup, hparams):
     scene, eids = setup
     for eid in eids:
         scene.set(eid, Position(), Velocity())
 
-
-@benchmark("mecs", "add", "3 components", setup=mecs_setup_empty)
 def mecs_add_three_components(setup, hparams):
     scene, eids = setup
     for eid in eids:
         scene.set(eid, Position(), Velocity(), Lifetime())
 
-@benchmark("mecs", "replace", "1 component", setup=mecs_setup_full)
 def mecs_overwrite_one_component(setup, hparams):
     scene, eids = setup
     for eid in eids:
         scene.set(eid, Position())
 
-@benchmark("mecs", "replace", "2 components", setup=mecs_setup_full)
 def mecs_overwrite_two_components(setup, hparams):
     scene, eids = setup
     for eid in eids:
         scene.set(eid, Position(), Velocity())
 
-
-@benchmark("mecs", "replace", "3 components", setup=mecs_setup_full)
 def mecs_overwrite_three_components(setup, hparams):
     scene, eids = setup
     for eid in eids:
         scene.set(eid, Position(), Velocity(), Lifetime())
 
-@benchmark("mecs", "has", "1 component", setup=mecs_setup_full)
 def mecs_has_one_component(setup, hparams):
     scene, eids = setup
     for eid in eids:
         scene.has(eid, Position)
 
-@benchmark("mecs", "has", "2 components", setup=mecs_setup_full)
 def mecs_has_two_components(setup, hparams):
     scene, eids = setup
     for eid in eids:
         scene.has(eid, Position, Velocity)
 
-@benchmark("mecs", "has", "3 components", setup=mecs_setup_full)
 def mecs_has_three_components(setup, hparams):
     scene, eids = setup
     for eid in eids:
         scene.has(eid, Position, Velocity, Lifetime)
 
-@benchmark("mecs", "has", "3 components (false)", setup=mecs_setup_partial)
 def mecs_has_three_components_false(setup, hparams):
     scene, eids = setup
     for eid in eids:
         scene.has(eid, Position, Velocity, Lifetime)
 
-@benchmark("mecs", "get", "1 component", setup=mecs_setup_full)
 def mecs_get_one_component(setup, hparams):
     scene, eids = setup
     for eid in eids:
         scene.get(eid, Position)
 
-@benchmark("mecs", "get", "2 components", setup=mecs_setup_full)
 def mecs_get_two_components(setup, hparams):
     scene, eids = setup
     for eid in eids:
         scene.collect(eid, Position, Velocity)
 
-@benchmark("mecs", "get", "3 components", setup=mecs_setup_full)
 def mecs_get_three_components(setup, hparams):
     scene, eids = setup
     for eid in eids:
         scene.collect(eid, Position, Velocity, Lifetime)
 
-@benchmark("mecs", "remove", "1 component", setup=mecs_setup_full)
 def mecs_remove_one_component(setup, hparams):
     scene, eids = setup
     for eid in eids:
         scene.remove(eid, Position)
 
-@benchmark("mecs", "remove", "2 components", setup=mecs_setup_full)
 def mecs_remove_two_components(setup, hparams):
     scene, eids = setup
     for eid in eids:
         scene.remove(eid, Position, Velocity)
 
-@benchmark("mecs", "remove", "3 components", setup=mecs_setup_full)
 def mecs_remove_three_components(setup, hparams):
     scene, eids = setup
     for eid in eids:
         scene.remove(eid, Position, Velocity, Lifetime)
 
-@benchmark("mecs", "destroy", "0 components", setup=mecs_setup_empty)
 def mecs_destroy_zero_components(setup, hparams):
     scene, eids = setup
     for eid in eids:
         scene.free(eid)
 
-@benchmark("mecs", "destroy", "3 components", setup=mecs_setup_full)
 def mecs_destroy_three_components(setup, hparams):
     scene, eids = setup
     for eid in eids:
         scene.free(eid)
 
-@benchmark("mecs", "archetype", "3 components", setup=mecs_setup_full)
 def mecs_archetype_three_components(setup, hparams):
     scene, eids = setup
     for eid in eids:
         scene.archetype(eid)
 
-@benchmark("mecs", "components", "3 components", setup=mecs_setup_full)
 def mecs_components_three_components(setup, hparams):
     scene, eids = setup
     for eid in eids:
         scene.components(eid)
 
-@benchmark("mecs", "example", "movement")
 def mecs_example_movement(setup, hparams):
     class MovementSystem():
         def onUpdate(self, scene, deltaTime, **kwargs):
@@ -275,7 +228,6 @@ def mecs_example_movement(setup, hparams):
         scene.new(Position(random.random() * 200 - 100, random.random() * 200 - 100), Velocity(random.random() * 10 - 5, random.random() * 10 - 5))
 
     scene.update(*systems, deltaTime=.1)
-
 
 def esper_setup_empty(hparams):
     world = esper.World()
@@ -321,43 +273,36 @@ def esper_setup_mixed(hparams):
     return world, eids
 
 
-@benchmark("esper", "create", "0 components", setup=lambda hparams: (esper.World(), []))
 def esper_create_zero_components(setup, hparams):
     world, _ = setup
     for _ in range(hparams.count):
         world.create_entity()
 
-@benchmark("esper", "create", "1 component", setup=lambda hparams: (esper.World(), []))
 def esper_create_one_component(setup, hparams):
     world, _ = setup
     for _ in range(hparams.count):
         world.create_entity(Position())
 
-@benchmark("esper", "create", "2 components", setup=lambda hparams: (esper.World(), []))
 def esper_create_two_components(setup, hparams):
     world, _ = setup
     for _ in range(hparams.count):
         world.create_entity(Position(), Velocity())
 
-@benchmark("esper", "create", "3 components", setup=lambda hparams: (esper.World(), []))
 def esper_create_three_components(setup, hparams):
     world, _ = setup
     for _ in range(hparams.count):
         world.create_entity(Position(), Velocity(), Lifetime())
 
-@benchmark("esper", "query", "1 component", setup=esper_setup_mixed)
 def esper_query_one_component(setup, hparams):
     world, _ = setup
     for _, _ in world.get_component(Position):
         pass
 
-@benchmark("esper", "query", "2 components", setup=esper_setup_mixed)
 def esper_query_two_components(setup, hparams):
     world, _ = setup
     for _, _ in world.get_components(Position, Velocity):
         pass
 
-@benchmark("esper", "query", "3 components", setup=esper_setup_mixed)
 def esper_query_three_components(setup, hparams):
     world, _ = setup
     for _, _ in world.get_components(Position, Velocity, Lifetime):
@@ -370,57 +315,81 @@ def main():
     parser.add_argument("-t", "--task", dest="task", default=None, type=str,
             help="The tasks to benchmark, separated by comma.")
     parser.add_argument("-c", "--count", dest="count", default=10**5, type=int,
-            help="The number of entities to benchmark (default 10**5)")
+            help="The number of entities to benchmark (default 10**5).")
+    parser.add_argument("-r", "--retries", dest="retries", type=int, default=10,
+            help="How many retries for each individual benchmark (default 10).")
     hparams = parser.parse_args()
 
-    functions = [
-        mecs_create_entities,
-        mecs_create_entities_one_component,
-        mecs_create_entities_two_components,
-        mecs_create_entities_three_components,
-        mecs_query_all_entities,
-        mecs_query_one_component,
-        mecs_query_two_components,
-        mecs_query_three_components,
-        mecs_add_one_component,
-        mecs_add_two_components,
-        mecs_add_three_components,
-        mecs_overwrite_one_component,
-        mecs_overwrite_two_components,
-        mecs_overwrite_three_components,
-        mecs_has_one_component,
-        mecs_has_two_components,
-        mecs_has_three_components,
-        mecs_has_three_components_false,
-        mecs_get_one_component,
-        mecs_get_two_components,
-        mecs_get_three_components,
-        mecs_remove_one_component,
-        mecs_remove_two_components,
-        mecs_remove_three_components,
-        mecs_destroy_zero_components,
-        mecs_destroy_three_components,
-        mecs_archetype_three_components,
-        mecs_components_three_components,
-        mecs_example_movement,
+    mecs_backend = ("mecs", mecs.__version__)
+    esper_backend = ("esper", esper.version)
 
-        esper_create_zero_components,
-        esper_create_one_component,
-        esper_create_two_components,
-        esper_create_three_components,
-        esper_query_one_component,
-        esper_query_two_components,
-        esper_query_three_components
+    benchmarks = [
+        # backend_name, task_name, specific_name, setup_method, benchmark_method
+        (mecs_backend, "create", "", mecs_setup_none, mecs_create_entities),
+        (mecs_backend, "create", "1 component", mecs_setup_none, mecs_create_entities_one_component),
+        (mecs_backend, "create", "2 components", mecs_setup_none, mecs_create_entities_two_components),
+        (mecs_backend, "create", "3 components", mecs_setup_none, mecs_create_entities_three_components),
+        (mecs_backend, "query", "all", mecs_setup_full, mecs_query_all_entities),
+        (mecs_backend, "query", "1 component", mecs_setup_full, mecs_query_one_component),
+        (mecs_backend, "query", "2 components", mecs_setup_full, mecs_query_two_components),
+        (mecs_backend, "query", "3 components", mecs_setup_full, mecs_query_three_components),
+        (mecs_backend, "add", "1 component", mecs_setup_empty, mecs_add_one_component),
+        (mecs_backend, "add", "2 components", mecs_setup_empty, mecs_add_two_components),
+        (mecs_backend, "add", "3 components", mecs_setup_empty, mecs_add_three_components),
+        (mecs_backend, "overwrite", "1 component", mecs_setup_full, mecs_overwrite_one_component),
+        (mecs_backend, "overwrite", "2 components", mecs_setup_full, mecs_overwrite_two_components),
+        (mecs_backend, "overwrite", "3 components", mecs_setup_full, mecs_overwrite_three_components),
+        (mecs_backend, "has", "1 component", mecs_setup_full, mecs_has_one_component),
+        (mecs_backend, "has", "2 components", mecs_setup_full, mecs_has_two_components),
+        (mecs_backend, "has", "3 components", mecs_setup_full, mecs_has_three_components),
+        (mecs_backend, "has", "3 components (false)", mecs_setup_full, mecs_has_three_components_false),
+        (mecs_backend, "get", "1 component", mecs_setup_full, mecs_get_one_component),
+        (mecs_backend, "get", "2 components", mecs_setup_full, mecs_get_two_components),
+        (mecs_backend, "get", "3 components", mecs_setup_full, mecs_get_three_components),
+        (mecs_backend, "remove", "1 component", mecs_setup_full, mecs_remove_one_component),
+        (mecs_backend, "remove", "2 components", mecs_setup_full, mecs_remove_two_components),
+        (mecs_backend, "remove", "3 components", mecs_setup_full, mecs_remove_three_components),
+        (mecs_backend, "destroy", "0 components", mecs_setup_full, mecs_destroy_zero_components),
+        (mecs_backend, "destroy", "3 components", mecs_setup_full, mecs_destroy_three_components),
+        (mecs_backend, "archetype", "", mecs_setup_full, mecs_archetype_three_components),
+        (mecs_backend, "components", "", mecs_setup_full, mecs_components_three_components),
+        (mecs_backend, "example", "movement", mecs_setup_full, mecs_example_movement),
+
+        (esper_backend, "create", "0 components", esper_setup_empty, esper_create_zero_components),
+        (esper_backend, "create", "1 component", esper_setup_empty, esper_create_one_component),
+        (esper_backend, "create", "2 components", esper_setup_empty, esper_create_two_components),
+        (esper_backend, "create", "3 components", esper_setup_empty, esper_create_three_components),
+        (esper_backend, "query", "1 component", esper_setup_full, esper_query_one_component),
+        (esper_backend, "query", "2 components", esper_setup_full, esper_query_two_components),
+        (esper_backend, "query", "3 components", esper_setup_full, esper_query_three_components)
     ]
 
-    if hparams.backend is not None:
-        backends = hparams.backend.split(",")
-    if hparams.task is not None:
-        tasks = hparams.task.split(",")
+    backends = hparams.backend.split(",") if hparams.backend is not None else None
+    tasks = hparams.task.split(",") if hparams.task is not None else None
 
-    for f in functions:
-        if (hparams.backend is None or f.backend in backends) and (hparams.task is None or f.task in tasks):
-            f(hparams, verbose=True)
+    for (backend, task, specific, setup, benchmark) in benchmarks:
+        backend_name, backend_version = backend
+        if backends is not None and backend_name not in backends: continue
+        if tasks is not None and task not in tasks: continue
+
+        print(f"{backend_name} ({backend_version})/{task}/{specific}: ", end="", flush=True)
+
+        times = []
+        for _ in range(hparams.retries):
+            print(".", end="", flush=True)
+
+            s = setup(hparams)
+
+            start = time.process_time()
+            benchmark(s, hparams)
+            end = time.process_time()
+
+            times.append(end - start)
+
+        m = mean(times)
+        print()
+        print(f"  mean: {m:<8f}s var: {variance(times, m):<8f}s min: {min(times):<8f}s max: {max(times):<8f}s")
+        print()
 
 if __name__ == "__main__":
     main()
